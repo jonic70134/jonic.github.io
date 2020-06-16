c1e8d2d2a5ddaf9efdc077888f86c9e429aeae77
Orange <jonic70134@gmail.com>
2020-06-16 16:22:34 +0800

    add tree_climber single page

---
+++ tree_climber/js/index.js
@@ -0,0 +1,16 @@
+
+	//index slider
+	var mySwiper = new Swiper ('.slider .swiper-container', {
+		loop: true,
+		autoHeight: true,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	
+	})
+
+
