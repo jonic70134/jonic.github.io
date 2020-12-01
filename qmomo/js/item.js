ca14526cb3bc5603233be128b4192aa7104dae7d
Orange <jonic70134@gmail.com>
2020-12-01 18:09:49 +0800

    add item color size tab

--- qmomo/js/item.js
+++ qmomo/js/item.js
@@ -34,6 +34,45 @@ jQuery(function($){
 		$("#"+detail_id).addClass('current');
 	});
 	
+	// 顏色選項 tab
+	$('.color > li').click(function(e){
+		e.preventDefault();
+		var color_conten_id = $(this).attr('data-color');
+		$('.color > li').removeClass('active');
+		$('.color_block > ul').removeClass('active');
+		$(this).addClass('active');
+		$("#"+color_conten_id).addClass('active');
+	});
+
+	// 尺寸選項 tab
+	$('.size_link > li').click(function(e){
+		e.preventDefault();
+		var size_conten_id = $(this).attr('data-size');
+		$('.size_link > li').removeClass('active');
+		$('.size_content').removeClass('active');
+		$(this).addClass('active');
+		$("#"+size_conten_id).addClass('active');
+	});
+	
+
+	// 商品數量加減
+	$('.quantity-decrease').click(function () {
+		var $input = $(this).parent().find('.quantity-input');
+		var count = parseInt($input.val()) - 1;
+		count = count < 1 ? 1 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+	$('.quantity-increase').click(function () {
+		var $input = $(this).parent().find('.quantity-input');
+		var count = parseInt($input.val()) + 1;
+		count = count > 5 ? 5 : count;
+		$input.val(count);
+		$input.change();
+		return false;
+	});
+
 });
 
 
