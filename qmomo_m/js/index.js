628f9c7eec9b530a5234cae55ad22a40bf6ae411
Orange <jonic70134@gmail.com>
2021-02-24 11:02:09 +0800

    add qmomo mobile template

---
+++ qmomo_m/js/index.js
@@ -0,0 +1,71 @@
+jQuery(function($){
+
+	// 首頁Slider
+	var bigslider = new Swiper('.big_slider .swiper-container', {
+		slidesPerView: 1,
+    	spaceBetween: 30,
+		loop: true,
+		autoplay: {
+			delay: 5000,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+
+
+	//浮動廣告slider
+	var popSlider = new Swiper('.pop_slider .swiper-container', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+	});
+	$(".pop-other").on('click', '.thumb', function(){
+		popSlider.slideTo($(this).index());	
+    });
+    $('.pop-other .thumb').on('click', function(){
+		$('.pop-other .thumb').removeClass('active');
+		$(this).addClass('active');
+	});	
+	$('.pop_slider .thumb').hover(function() {
+		$( this ).trigger( "click" );
+	});
+	$('.close_pop').click(function(){
+		$('.pop_slider').hide();
+	});
+
+
+	$('.foot_link_open').click(function(){
+		$('.float_block').slideDown();
+	});
+	// tab
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab_content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
+
+	//點選購物車彈出視窗選規格
+	$('.item .fa-shopping-cart').on('click', function(e) {
+		e.preventDefault();
+		$.fancybox.open({
+			src  : 'qmomo_fancybox.html#cart_popup',
+			type : 'ajax',
+		});
+	});
+});
+
+$("document").ready(function() {
+    setTimeout(function() {
+        $('#index_ad').trigger('click');
+    },10);
+});
