546edb6941252912f674ddb8bb07b6597109acaa
Orange <jonic70134@gmail.com>
2021-03-23 17:51:00 +0800

    新增購物車結帳頁 訂單明細頁

--- qmomo/js/cart_orderlist.js
+++ qmomo/js/cart_orderlist.js
@@ -34,5 +34,45 @@ jQuery(function($){
 	$('.alert_close').click(function () {
 		$('.alert_window').hide();
 	});
+
+	//7-11選擇門市
+	$('.deliver_711').on('click', function(e) {
+		e.preventDefault();
+		$('.deliver_711_popup').css('visibility','visible');
+		$('body').css('overflow','hidden');
+	});
+	$('.pop_close').on('click', function(e) {
+		e.preventDefault();
+		$('.deliver_711_popup').css('visibility','hidden');
+		$('body').css('overflow','auto');
+	});
+	$('.deliver_complete').on('click', function(e) {
+		e.preventDefault();
+		$('.deliver_711_popup').css('visibility','hidden');
+		$('body').css('overflow','auto');
+	});
+
+	// 全家選擇門市
+	$('.deliver_family').on('click', function(e) {
+		e.preventDefault();
+		$('.deliver_family_popup').css('visibility','visible');
+		$('body').css('overflow','hidden');
+	});
+	$('.pop_close').on('click', function(e) {
+		e.preventDefault();
+		$('.deliver_family_popup').css('visibility','hidden');
+		$('body').css('overflow','auto');
+	});
+	$('.deliver_complete').on('click', function(e) {
+		e.preventDefault();
+		$('.deliver_family_popup').css('visibility','hidden');
+		$('body').css('overflow','auto');
+	});
+	// $("input[name$='payment']").click(function() {
+    //     var paymentChoose = $(this).val();
+
+    //     $(".trans_con").hide();
+    //     $("#trans" + paymentChoose).show();
+    // });
 	
 });
