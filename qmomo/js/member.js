fd0623e97b898aeddcfe49c16e06bf632b676385
Orange <jonic70134@gmail.com>
2021-04-16 17:47:37 +0800

    會員新增常用收件人、常用統一編號、優惠券專區

--- qmomo/js/member.js
+++ qmomo/js/member.js
@@ -1,3 +1,29 @@
 jQuery(function($){
+	//根據點選的連結增加class=active
+	$('.mem_link a[href^="' + location.pathname.slice(1,9) + '"]').addClass('active');
+
+	//新增收件人&統一編號彈出視窗
+	$('.location_pop_btn').on('click', function(e) {
+		e.preventDefault();
+		$('.location_pop').css('visibility','visible');
+		$('body').css('overflow','hidden');
+	});
+	$('.cancel').on('click', function(e) {
+		e.preventDefault();
+		$('.location_pop').css('visibility','hidden');
+		$('body').css('overflow','auto');
+	});
+
+	// 優惠券tab
+	$('.coupon_detail a').click(function(e){
+		e.preventDefault();
+		var detail_id = $(this).attr('data-detail');
+
+		$('.coupon_detail a').removeClass('active');
+		$('.detail_content').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+detail_id).addClass('active');
+	}); 
 
 });
