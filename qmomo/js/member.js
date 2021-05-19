706ea139240feede08890e688ae4a8b925482c97
Orange <jonic70134@gmail.com>
2021-05-19 17:34:13 +0800

    修改密碼頁面整併至會員資料，改為tab方式呈現

--- qmomo/js/member.js
+++ qmomo/js/member.js
@@ -3,12 +3,22 @@ jQuery(function($){
 	$('.mem_link a[href^="' + location.pathname.slice(1,9) + '"]').addClass('active');
 
 	//新增收件人&統一編號彈出視窗
-	$('.location_pop_btn').on('click', function(e) {
+	$('#mem_point').click(function(e){
 		e.preventDefault();
-		$('.location_pop').css('visibility','visible');
+		$('.mem_point_pop').css('visibility','visible');
 		$('body').css('overflow','hidden');
 	});
-	$('.cancel').on('click', function(e) {
+	$('.location_pop_btn').click(function(e){
+		e.preventDefault();
+		$('#recipient_pop').css('visibility','visible');
+		$('body').css('overflow','hidden');
+	});
+	$('.mem_pop_btn').click(function(e){
+		e.preventDefault();
+		$('#mem_data_pop').css('visibility','visible');
+		$('body').css('overflow','hidden');
+	});
+	$('.location_pop .bot_func a').click(function(e) {
 		e.preventDefault();
 		$('.location_pop').css('visibility','hidden');
 		$('body').css('overflow','auto');
@@ -44,6 +54,18 @@ jQuery(function($){
 		var detail_id = $(this).attr('data-detail');
 
 		$('.mem_pra_detail a').removeClass('active');
+		$('.detail_content').removeClass('active');
+
+		$(this).addClass('active');
+		$("#"+detail_id).addClass('active');
+	}); 
+
+	// 會員資料tab
+	$('.mem_info_detail a').click(function(e){
+		e.preventDefault();
+		var detail_id = $(this).attr('data-detail');
+
+		$('.mem_info_detail a').removeClass('active');
 		$('.detail_content').removeClass('active');
 
 		$(this).addClass('active');
