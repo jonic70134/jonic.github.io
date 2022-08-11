b119b9d32b710dd3f3fc60cc3b7bde2f04e87ef0
Orange <jonic70134@gmail.com>
2022-08-11 11:29:23 +0800

    KOL add confirm & wrong msg page

--- kol/js/chk.js
+++ kol/js/chk.js
@@ -1,17 +1,6 @@
-var kol_slide = new Swiper('.kol_slide', {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'fraction',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
-
-
-// 愛心class切換
-$(".icon_track").click(function(e){
-    e.preventDefault();
-    $(this).toggleClass('icon_track icon_track_plus');
+$("input[name$='payway']").click(function() {
+  var paydiv = $(this).val();
+  console.log(paydiv)
+  $(".paydiv").hide();
+  $("#paydiv" + paydiv).show();
 });
