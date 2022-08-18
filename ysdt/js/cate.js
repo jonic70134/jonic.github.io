e93a65e8046f2ffd2a9839e67d0e582aba3c9cd3
Orange <jonic70134@gmail.com>
2022-08-18 16:38:00 +0800

    YSDT add cate pic/txt chang function / delete some no use files

---
+++ ysdt/js/cate.js
@@ -0,0 +1,13 @@
+
+// 智慧分類menu開合
+$('.nav-menu').hover(function() {
+    $('.menu-one').stop(true,true).slideToggle();
+});
+
+// 愛心class切換
+$(".icon_pictxt").click(function(e){
+    e.preventDefault();
+    $(this).toggleClass('icon_pictxt icon_picview');
+    $('.cate_items').toggleClass('cate_items_txt');
+    $('.relate_prod').toggle();
+});
