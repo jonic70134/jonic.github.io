571a55363d8223ba39490ea92706289ccd8a7d04
Orange <jonic70134@gmail.com>
2022-08-18 16:36:10 +0800

    KOL chk page add fill form alert & add index page

--- kol/js/chk.js
+++ kol/js/chk.js
@@ -1,6 +1,12 @@
 $("input[name$='payway']").click(function() {
   var paydiv = $(this).val();
-  console.log(paydiv)
   $(".paydiv").hide();
   $("#paydiv" + paydiv).show();
+});
+
+
+$("input[name$='invo']").click(function() {
+  var invo = $(this).val();
+  $(".invo_credit").hide();
+  $("#invo" + invo).show();
 });
---
+++ kol/js/item.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    },
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+});
+
+
+// 愛心class切換
+$(".icon_track").click(function(e){
+    e.preventDefault();
+    $(this).toggleClass('icon_track icon_track_plus');
+});
