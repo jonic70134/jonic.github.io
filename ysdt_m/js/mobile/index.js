08a296b4ec8ce12996cc6297a934cfe577836e1e
Orange <jonic70134@gmail.com>
2022-08-22 17:23:56 +0800

    [YSDT_m] add index page & img file

--- ysdt_m/js/mobile/index.js
+++ ysdt_m/js/mobile/index.js
@@ -1,10 +1,14 @@
-var galleryTop = new Swiper('.per .swiper-container', {
-    slidesPerView: 'auto',
-    spaceBetween: 5,
+var duo1slide = new Swiper('.duo_1 .swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 20,
+});
+var duo2slide = new Swiper('.duo_2 .swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
 });
 
 //menu
-$('.btn-menu').click(function(e) {
+$('.icon_menu').click(function(e) {
     e.preventDefault();
     $('.md_nav_menu').stop(true,false).toggleClass("active");
     $(".body_container").toggleClass("fixed");
@@ -13,14 +17,39 @@ $('.btn-menu').click(function(e) {
 });
 
 
-// 個人禮券slider
-var cupnslide = new Swiper('.cupn_slide.swiper-container', {
-    slidesPerView: 'auto',
+// 折價券slider
+var cupnslide = new Swiper('.cupn_slide.swiper', {
+    slidesPerView: '1.2',
     spaceBetween: 10,
-    freeMode: true,
-  });
+    grid: {
+        rows: 2,
+        fill: 'row',
+    },
+    breakpoints: {
+        430: {
+            slidesPerView: 1.2,
+        },
+        510: {
+            slidesPerView: 1.5,
+        },
+        600: {
+            slidesPerView: 1.8,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+        790: {
+            slidesPerView: 2.2,
+        }
+    }
+});
+// 特賣主題 slider
+var specslide = new Swiper('.spec_slide', {
+    slidesPerView: 1.25,
+    spaceBetween: 10,
+});
 
-$(".site-header .search").click(function(){
+$(".site-header .icon_search").click(function(){
     $(".site-search").toggle();
 });
 
