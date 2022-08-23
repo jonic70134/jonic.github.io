12868ac8eb54ada3c77e9abdf37ed0af2f86859e
Orange <jonic70134@gmail.com>
2022-08-23 17:23:56 +0800

    [YSDT_m] add menu and tag-cloud

--- ysdt_m/js/mobile/index.js
+++ ysdt_m/js/mobile/index.js
@@ -1,16 +1,16 @@
 var duo1slide = new Swiper('.duo_1 .swiper', {
-    slidesPerView: 1.2,
-    spaceBetween: 20,
+    slidesPerView: 'auto',
+    spaceBetween: 10,
 });
 var duo2slide = new Swiper('.duo_2 .swiper', {
-    slidesPerView: 1.2,
+    slidesPerView: 'auto',
     spaceBetween: 10,
 });
 
 //menu
 $('.icon_menu').click(function(e) {
     e.preventDefault();
-    $('.md_nav_menu').stop(true,false).toggleClass("active");
+    $('.menu').stop(true,false).toggleClass("active");
     $(".body_container").toggleClass("fixed");
     $("body").toggleClass("o_hidden");
     return false;
@@ -19,35 +19,29 @@ $('.icon_menu').click(function(e) {
 
 // 折價券slider
 var cupnslide = new Swiper('.cupn_slide.swiper', {
-    slidesPerView: '1.2',
+    slidesPerView: 'auto',
     spaceBetween: 10,
     grid: {
         rows: 2,
         fill: 'row',
     },
-    breakpoints: {
-        430: {
-            slidesPerView: 1.2,
-        },
-        510: {
-            slidesPerView: 1.5,
-        },
-        600: {
-            slidesPerView: 1.8,
-        },
-        700: {
-            slidesPerView: 2,
-        },
-        790: {
-            slidesPerView: 2.2,
-        }
-    }
+    
 });
 // 特賣主題 slider
 var specslide = new Swiper('.spec_slide', {
-    slidesPerView: 1.25,
+    slidesPerView: 1.2,
     spaceBetween: 10,
 });
+
+// // 熱搜 slider
+// var hotslide = new Swiper('.hot_slide', {
+//     slidesPerView: 5,
+//     spaceBetween: 10,
+//     grid: {
+//         rows: 1,
+//         fill: 'row',
+//     },
+// });
 
 $(".site-header .icon_search").click(function(){
     $(".site-search").toggle();
