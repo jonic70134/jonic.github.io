bb7d8ba854e0465cc1cb4f0e77dbdec604cfd51b
Orange <jonic70134@gmail.com>
2022-08-25 09:48:33 +0800

    [YSDT_m] add category change pic/txt view

---
+++ ysdt_m/js/mobile/cate.js
@@ -0,0 +1,22 @@
+
+//menu
+$('.btn-menu').click(function(e) {
+    e.preventDefault();
+    $('.menu').stop(true,false).toggleClass("active");
+    $(".body_container").toggleClass("fixed");
+    $("body").toggleClass("o_hidden");
+    return false;
+});
+
+// 圖文class切換
+$(".icon_pictxt").click(function(e){
+    e.preventDefault();
+    $(this).toggleClass('icon_pictxt icon_picview');
+    $('.cate_items').toggleClass('cate_items_txt');
+});
+
+// 愛心class切換
+$(".icon_track").click(function(e){
+    e.preventDefault();
+    $(this).toggleClass('icon_track icon_track_plus');
+});
