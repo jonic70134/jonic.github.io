5d1552a0aa6dd850f3665d196e3abecd7ee05db0
Orange <jonic70134@gmail.com>
2022-08-31 11:01:45 +0800

    [YSDT] change cate page html architecture & style , add crazy & fresh page

--- ysdt/js/cate.js
+++ ysdt/js/cate.js
@@ -9,5 +9,4 @@ $(".icon_pictxt").click(function(e){
     e.preventDefault();
     $(this).toggleClass('icon_pictxt icon_picview');
     $('.cate_items').toggleClass('cate_items_txt');
-    $('.relate_prod').toggle();
 });
