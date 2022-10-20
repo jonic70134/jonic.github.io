b6300ebe8a6e1df21553f9f9e0fedc0f43265b9e
Orange <jonic70134@gmail.com>
2022-10-20 14:44:48 +0800

    [YSDT_m] menu tab use pure css without js

--- ysdt_m/js/mobile/index.js
+++ ysdt_m/js/mobile/index.js
@@ -1,12 +1,3 @@
-// var duo1slide = new Swiper('.duo_1 .swiper', {
-//     slidesPerView: 'auto',
-//     spaceBetween: 10,
-// });
-// var duo2slide = new Swiper('.duo_2 .swiper', {
-//     slidesPerView: 'auto',
-//     spaceBetween: 10,
-// });
-
 //menu
 $('.icon_menu').click(function(e) {
     e.preventDefault();
@@ -16,32 +7,6 @@ $('.icon_menu').click(function(e) {
     return false;
 });
 
-
-// 折價券slider
-// var cupnslide = new Swiper('.cupn_slide.swiper', {
-//     slidesPerView: 'auto',
-//     spaceBetween: 10,
-//     grid: {
-//         rows: 2,
-//         fill: 'row',
-//     },
-    
-// });
-// 特賣主題 slider
-// var specslide = new Swiper('.spec_slide', {
-//     slidesPerView: 1.2,
-//     spaceBetween: 10,
-// });
-
-// // 熱搜 slider
-// var hotslide = new Swiper('.hot_slide', {
-//     slidesPerView: 5,
-//     spaceBetween: 10,
-//     grid: {
-//         rows: 1,
-//         fill: 'row',
-//     },
-// });
 
 $(".site-header .icon_search").click(function(){
     $(".site-search").toggle();
