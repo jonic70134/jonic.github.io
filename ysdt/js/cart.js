baec646d4e9952406f2d5d7dcc34842a10ae6af7
Orange <jonic70134@gmail.com>
2022-12-12 15:38:11 +0800

    [YSDT] cart add confirm page

--- ysdt/js/cart.js
+++ ysdt/js/cart.js
@@ -1,3 +1,16 @@
+// orderlist
 $("#order_chk_all").click(function(){
     $('input:checkbox').not(this).prop('checked', this.checked);
 });
+
+// confirm
+
+$(document).ready(function() {
+    $("input[name$='deliver_way']").click(function() {
+        var deliverbox = $(this).val();
+
+        $(".deliver_block").hide();
+        $("#deliver_block" + deliverbox).show();
+    });
+});
+
