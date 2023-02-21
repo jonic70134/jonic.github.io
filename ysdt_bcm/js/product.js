60c864003ebb24ec8643d0557cc4a9cb84d5cc5a
jonic70134 <jonic70134@gmail.com:wqwqqwqwq>
2023-02-21 16:52:24 +0800

    [YSDT_BCM] add product page add  wysihtml & empty certified

--- ysdt_bcm/js/product.js
+++ ysdt_bcm/js/product.js
@@ -1,14 +1,17 @@
+$('.add-product-detail-form input').blur(function(){
+    if( !$(this).val() ) {
+          $(this).parents('.form-content').addClass('warning');
+    }else{
+        $(this).parents('.form-content').removeClass('warning');
+    }
+});
 // 上傳圖片
-
-
-// 拖放事件
 const dropzone = $('#file-input');
 const dropframe = $('.chos-file');
 dropzone.on('dragover', function(e) {
     e.preventDefault();
     dropzone.addClass('dragover');
 });
-
 dropzone.on('drop', function(e) {
     e.preventDefault();
     dropzone.removeClass('dragover');
@@ -17,8 +20,6 @@ dropzone.on('drop', function(e) {
 $('#file-input').on('change', function() {
     handleFiles($('#file-input').prop('files'));
 });
-
-
 // 選擇檔案按鈕被選擇時觸發
 function handleFiles(files) {
     for (let i = 0; i < files.length; i++) {
@@ -32,8 +33,8 @@ function handleFiles(files) {
                 const wrapper = $('<div class="preview-image-wrapper"/>').append(img, delBtn);
                 // 將預覽圖片包裝器添加到預覽區域
                 $('.preview').append(wrapper);
-                    // 當刪除按鈕被點擊時，刪除對應的預覽圖片包裝器
-                    delBtn.on('click', function() {
+                // 當刪除按鈕被點擊時，刪除對應的預覽圖片包裝器
+                delBtn.on('click', function() {
                     wrapper.remove();
                 });
             };
@@ -44,7 +45,31 @@ function handleFiles(files) {
             setTimeout(function() {dropframe.removeClass('error_type')}, 5000);
         }
     }
-    
+}
+
+// 上傳影片
+$('#video_pop .box_btn_send').on('click', function(e) {
+    var youtubeLink = $('#video-input').val();
+    var embeddedLink = youtubeLink.replace("embed/");
+    if (isValidYoutubeLink(youtubeLink)) {
+        $('#preview-video').attr('src', embeddedLink);
+        $('.video-container').show();
+        $('#video_pop .con_block').removeClass('error_link');
+    }else {
+        // 顯示錯誤訊息
+        e.preventDefault();
+        $('#video_pop .con_block').addClass('error_link');
+    }
+});
+$('.preview-video-delete').on('click', function() {
+    $('.video-container').hide();
+    $('#preview-video').attr('src', '');
+});
+
+// 檢查yoube網址
+function isValidYoutubeLink(link) {
+    var pattern = /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+$/;
+    return pattern.test(link);
 }
 
 // 切換字功能
@@ -80,4 +105,12 @@ $(".fa-circle-plus").click( function(e){
 
 $(".fa-trash-can").click( function(e){
     e.preventDefault();
+});
+
+
+//商品介紹 wysihtml
+var editor = new Quill('#editor', {
+    modules: { toolbar: '#toolbar' },
+    theme: 'snow',
+    placeholder: '請輸入商品說明',
 });
