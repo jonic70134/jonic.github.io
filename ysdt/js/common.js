487140169b595b07b0fda2d4399a5eaa628b19b4
jonic70134 <jonic70134@gmail.com>
2023-03-25 10:38:25 +0800

    [btype] update supplier page, update menu txt ,  update head powerby close btn to block, add new feture  [detect the largest area color of the picture and fill in the text] , add footer  RWD

--- ysdt/js/common.js
+++ ysdt/js/common.js
@@ -9,6 +9,72 @@ var tagslide = new Swiper('.tag-cloud', {
     loop:true,
 });
 
+$(function() {
+    var $img = $('.logo img');
+    var blockSize = 5;
+    var defaultRGB = {r:0,g:0,b:0};
+    var canvas = document.createElement('canvas');
+    var context = canvas.getContext && canvas.getContext('2d');
+    var data, width, height, i = -4, length, rgb = {r:0,g:0,b:0}, count = 0;
+    var $pby = $('.pby');
+    var $pbybtn = $('.pbybtn');
+    if (!context) {
+      return defaultRGB;
+    }
+  
+    height = canvas.height = $img[0].naturalHeight || $img[0].offsetHeight || $img[0].height;
+    width = canvas.width = $img[0].naturalWidth || $img[0].offsetWidth || $img[0].width;
+  
+    context.drawImage($img[0], 0, 0);
+  
+    try {
+      data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+      /* security error, img on diff domain */
+    //   alert('x');
+      return defaultRGB;
+    }
+  
+    length = data.data.length;
+  
+    var maxArea = 0;
+    var maxRGB = {r:0,g:0,b:0};
+    var areas = {};
+  
+    while ( (i += blockSize * 4) < length ) {
+        var area = Math.floor((i + 4) / (blockSize * 4));
+        var red = data.data[i];
+        var green = data.data[i + 1];
+        var blue = data.data[i + 2];
+        var alpha = data.data[i + 3];
+    
+        if (alpha > 0) {
+            if (!areas[area]) {
+                areas[area] = {r:0,g:0,b:0,count:0};
+            }
+    
+            areas[area].r += red;
+            areas[area].g += green;
+            areas[area].b += blue;
+            areas[area].count++;
+    
+            if (areas[area].count > maxArea) {
+                maxArea = areas[area].count;
+                maxRGB = areas[area];
+            }
+        }
+    }
+  
+    // ~~ used to floor values
+    maxRGB.r = ~~(maxRGB.r / maxRGB.count);
+    maxRGB.g = ~~(maxRGB.g / maxRGB.count);
+    maxRGB.b = ~~(maxRGB.b / maxRGB.count);
+  
+    $pby.css('color', 'rgb('+maxRGB.r+','+maxRGB.g+','+maxRGB.b+')');
+    $pbybtn.css('color', 'rgb('+maxRGB.r+','+maxRGB.g+','+maxRGB.b+')');
+  });
+  
+
 // power by
 var removeClass = true;
 $('.pbybtn').click(function(e) {
