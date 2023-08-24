07c94d4c5beb46c3a2556a9602c874b3eb3d8bb1
jonic70134 <jonic70134@gmail.com>
2023-08-25 07:42:44 +0800

    [b_pc] add mem_discout page & cart discound option update & intro page description add gift pic

--- b_pc/site_staff/js/cart.js
+++ b_pc/site_staff/js/cart.js
@@ -1,18 +1,18 @@
 // orderlist
-$("#order_chk_all").click(function(){
+$("#order_chk_all").click(function () {
     $('input:checkbox').not(this).prop('checked', this.checked);
 });
 
 // chk
 
-$("input[name$='payway']").click(function() {
+$("input[name$='payway']").click(function () {
     var paydiv = $(this).val();
     $(".paydiv").hide();
     $("#paydiv" + paydiv).show();
 });
-  
-  
-$("input[name$='invo']").click(function() {
+
+
+$("input[name$='invo']").click(function () {
     var invo = $(this).val();
     $(".invo_credit").hide();
     $("#invo" + invo).show();
@@ -20,9 +20,31 @@ $("input[name$='invo']").click(function() {
 
 
 //看以前加入購物車的商品
-$('.add_before').click(function(e) {
+$('.add_before').click(function (e) {
     e.preventDefault();
-    $('.cart_item.is-before').css('display','flex');
+    $('.cart_item.is-before').css('display', 'flex');
     $('.add_before').hide();
     $('.before_t p').show();
+});
+
+// 選擇折價券
+$(".cart_cupn_block .box_btn").click(function () {
+    $(".cupn_price").fadeIn(200);
+});
+
+// 使用折扣碼
+$('.cart_cupn_text').on('input', function () {
+    $('.code_btn_submit').prop('disabled', !$(this).val());
+});
+
+$('.code_btn_submit').click(function () {
+    $(".code_price").fadeIn(200);
+    $(".code_btn_cancel").show();
+});
+$(".code_btn_cancel").click(function (e) {
+    e.preventDefault();
+    $(".code_price").hide();
+    $(this).hide();
+    $('.cart_cupn_text').val();
+
 });
